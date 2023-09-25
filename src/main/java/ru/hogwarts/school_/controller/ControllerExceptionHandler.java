f44f1f5ba3d981c6c3534585bbba864d6dba3a2b
Saw3rtt <sawertpra@gmail.com>
2023-09-26 01:56:24 +0300

    task 4.4 done!

--- src/main/java/ru/hogwarts/school_/controller/ControllerExceptionHandler.java
+++ src/main/java/ru/hogwarts/school_/controller/ControllerExceptionHandler.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school_.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,10 +11,23 @@ import ru.hogwarts.school_.exception.StudentException;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
+
+    Logger logger = LoggerFactory.getLogger(ControllerExceptionHandler.class);
+
     @ExceptionHandler({StudentException.class, FacultyException.class})
     public ResponseEntity<String> handleStudentException(RuntimeException ex) {
+        logger.warn(ex.getMessage());
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(ex.getMessage());
     }
+
+    @ExceptionHandler({Exception.class})
+    public ResponseEntity<String> handleException(Exception ex) {
+        logger.error(String.valueOf(ex));
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body("Ошибка сервера, приносим извинения");
+    }
 }
+
